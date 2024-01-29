1e3609fe7a5ac2764a1f9f12e33c4c96a57e560c
N3ctar17 <kc.endrijaitis@gmail.com>
2024-01-29 16:25:16 -0600

    Added print statement to line 6

--- Quiz3_Code.py
+++ Quiz3_Code.py
@@ -3,7 +3,7 @@ def multi(num1: int, num2: int) -> int:
     return num1 * num2
 
 def main():
-    # Multiply two numbers
+    print("Starting Multiplication")
     print(multi(9, 8))
 
 if __name__ == "__main__":
