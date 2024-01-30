17300ed7c4debed88b08d3d4928cec480dc4e5dd
Brandon Sharp <bmssharp2002@gmail.com>
2024-01-29 18:47:06 -0600

    Adding file is fun

--- Quiz3_Code.py
+++ Quiz3_Code.py
@@ -5,7 +5,7 @@ def multi(num1: int, num2: int) -> int:
 def main():
     print("Starting Multiplication")
     print(multi(20, 11))
-    print("Done")
+    print("Fun")
 
 
 if __name__ == "__main__":
