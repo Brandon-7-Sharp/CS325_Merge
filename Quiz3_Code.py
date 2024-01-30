85e91087bb2fcf343b56f34a32aba04b08796973
N3ctar17 <156962766+N3ctar17@users.noreply.github.com>
2024-01-29 19:53:35 -0600

    changed values in line 8

--- Quiz3_Code.py
+++ Quiz3_Code.py
@@ -1,12 +1,11 @@
 def multi(num1: int, num2: int) -> int:
-    print("Starting")
+    print(str(num1) + "*" + str(num2))
     return num1 * num2
 
 def main():
     print("Starting Multiplication")
-    print(multi(20, 11))
-    print("Fun")
-
+    
+    print(multi(20, 4))
 
 if __name__ == "__main__":
     main()
