61ca2c9e1a7f785188a200febc5203fd04a0ffe1
Brandon Sharp <93329974+Brandon-7-Sharp@users.noreply.github.com>
2024-01-29 18:44:43 -0600

    Update Quiz3_Code.py

--- Quiz3_Code.py
+++ Quiz3_Code.py
@@ -1,12 +1,12 @@
 def multi(num1: int, num2: int) -> int:
-    print("Calculate")
+    print("Starting")
     return num1 * num2
 
 def main():
     print("Starting Multiplication")
     print(multi(20, 11))
-    print("Hello")
-    print("I love this")
+    
+    print("Done")
 
 if __name__ == "__main__":
     main()
